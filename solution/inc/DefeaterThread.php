c583e1fb6be37299973efa0896d40073f840dc0d
Mikk Tendermann <mikk150@gmail.com>
2016-08-04 00:15:55 +0300

    catch exeptions, and if universe is dead, then mark it as garbage

--- solution/inc/DefeaterThread.php
+++ solution/inc/DefeaterThread.php
@@ -13,52 +13,57 @@ class DefeaterThread extends Collectable
 
     public function run()
     {
+        try {
+            
+            $loop = \React\EventLoop\Factory::create();
+            $handler = new \WyriHaximus\React\GuzzlePsr7\HttpClientAdapter($loop);
 
-        $loop = \React\EventLoop\Factory::create();
-        $handler = new \WyriHaximus\React\GuzzlePsr7\HttpClientAdapter($loop);
+            $client = new \GuzzleHttp\Client([
+                'handler' => \GuzzleHttp\HandlerStack::create($handler),
+            ]);
+            $defeatedKnights = 0;
+            for ($knight = 0; $knight < $this->defeatKnights; $knight++) {
+                $client->getAsync('http://www.dragonsofmugloar.com/api/game')->then(function ($gameResponse) use ($client, &$defeatedKnights) {
+                    $game = new Game(json_decode($gameResponse->getBody()->getContents(), true));
 
-        $client = new \GuzzleHttp\Client([
-            'handler' => \GuzzleHttp\HandlerStack::create($handler),
-        ]);
-        $defeatedKnights = 0;
-        for ($knight = 0; $knight < $this->defeatKnights; $knight++) {
-            $client->getAsync('http://www.dragonsofmugloar.com/api/game')->then(function ($gameResponse) use ($client, &$defeatedKnights) {
-                $game = new Game(json_decode($gameResponse->getBody()->getContents(), true));
+                    $client->getAsync('http://www.dragonsofmugloar.com/weather/api/report/' . $game->gameId)->then(function ($weatherResponse) use ($client, $game, &$defeatedKnights) {
+                        $weather = $weatherResponse->getBody()->getContents();
+                        $weather=simplexml_load_string($weather);
 
-                $client->getAsync('http://www.dragonsofmugloar.com/weather/api/report/' . $game->gameId)->then(function ($weatherResponse) use ($client, $game, &$defeatedKnights) {
-                    $weather = $weatherResponse->getBody()->getContents();
-                    $weather=simplexml_load_string($weather);
+                        $dragon = new Dragon();
+                        $dragon->setKnightSkills($game->getKnight());
 
-                    $dragon = new Dragon();
-                    $dragon->setKnightSkills($game->getKnight());
+                        $dragonSkills = $dragon->attack((string) $weather->code);
 
-                    $dragonSkills = $dragon->attack((string) $weather->code);
-
-                    if ($dragonSkills === false) {
-                        // No dragon flies in storm! No knight fights in storm. My neighbor even doesn't send the dog out.
-                        $defeatedKnights++;
-                        echo $this->thread.' knights slaughtered: '.$defeatedKnights . PHP_EOL;
-                        if ($defeatedKnights == $this->defeatKnights) {
-                            $this->setGarbage(); //set current thread as garbage, because all knights in this universe are slaughtered
+                        if ($dragonSkills === false) {
+                            // No dragon flies in storm! No knight fights in storm. My neighbor even doesn't send the dog out.
+                            $defeatedKnights++;
+                            echo $this->thread.' knights slaughtered: '.$defeatedKnights . PHP_EOL;
+                            if ($defeatedKnights == $this->defeatKnights) {
+                                $this->setGarbage(); //set current thread as garbage, because all knights in this universe are slaughtered
+                            }
+                            return;
                         }
-                        return;
-                    }
 
-                    $client->putAsync('http://www.dragonsofmugloar.com/api/game/'.$game->gameId.'/solution', [
-                        'json' => ['dragon' => $dragonSkills],
-                    ])->then(function ($battleResponse) use ($client, $game, &$defeatedKnights) {
-                        echo $this->thread.' knight slaughtering reason: '.$battleResponse->getBody()->getContents() . PHP_EOL;
-                        $defeatedKnights++;
-                        echo $this->thread.' knights slaughtered: '.$defeatedKnights . PHP_EOL;
-                        if ($defeatedKnights == $this->defeatKnights) {
-                            $this->setGarbage(); //set current thread as garbage, because all knights in this universe are slaughtered
-                        }
+                        $client->putAsync('http://www.dragonsofmugloar.com/api/game/'.$game->gameId.'/solution', [
+                            'json' => ['dragon' => $dragonSkills],
+                        ])->then(function ($battleResponse) use ($client, $game, &$defeatedKnights) {
+                            echo $this->thread.' knight slaughtering reason: '.$battleResponse->getBody()->getContents() . PHP_EOL;
+                            $defeatedKnights++;
+                            echo $this->thread.' knights slaughtered: '.$defeatedKnights . PHP_EOL;
+                            if ($defeatedKnights == $this->defeatKnights) {
+                                $this->setGarbage(); //set current thread as garbage, because all knights in this universe are slaughtered
+                            }
+                        });
                     });
                 });
-            });
-            #echo 'created game '. $i.PHP_EOL;
-        }
+                #echo 'created game '. $i.PHP_EOL;
+            }
 
         $loop->run();
+        } catch (Exception $e) {
+            var_dump($e);
+            $this->setGarbage(); //set this thread as garbage, because it basically died anyway
+        }
     }
 }
